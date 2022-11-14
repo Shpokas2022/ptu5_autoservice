71056bc7e31ed40d77a4ccab4a19f48b48d22ee4
Kestutis Januskevicius <infohata@gmail.com>
2022-11-14 10:46:34 +0200

    car list and detail functional views

--- ptu5_autoservice/autoservice/urls.py
+++ ptu5_autoservice/autoservice/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('cars/', views.car_list_view, name='cars'),
+    path('car/<int:pk>/', views.car_detail_view, name='car'),
 ]
--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 
@@ -7,5 +7,15 @@ def index(request):
         'cars_count': models.Car.objects.count(),
         'services_count': models.Service.objects.count(),
         'orders_count': models.Order.objects.count(),
+    })
+
+def car_list_view(request):
+    return render(request, 'autoservice/car_list.html', {
+        'car_list': models.Car.objects.all()
+    })
+
+def car_detail_view(request, pk):
+    return render(request, 'autoservice/car_detail.html', {
+        'object': get_object_or_404(models.Car, pk=pk),
     })
 
